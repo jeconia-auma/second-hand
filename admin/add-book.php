af07c74d3c1de33839929bf702b02195b1d56341
Knight <knight@localhost.localdomain>
2022-09-25 16:23:45 +0300

    created empty files i.e: add-book, delete-book update-book Commit done by okoth jeconia auma

---
+++ admin/add-book.php
@@ -0,0 +1,9 @@
+<?php include("partials/menu.php"); ?>
+
+<div class="container">
+    <div class="wrapper">
+        <h1>Add Books</h1>
+    </div>
+</div>
+
+<?php include("partials/footer.php"); ?>
---
+++ admin/update-book.php
@@ -0,0 +1,9 @@
+<?php include("partials/menu.php"); ?>
+
+<div class="container">
+    <div class="wrapper">
+        <h1>Update Books</h1>
+    </div>
+</div>
+
+<?php include("partials/footer.php"); ?>
