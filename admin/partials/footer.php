aa934e3d69a938959218ddbecba9f006cb7c6975
Knight <knight@localhost.localdomain>
2022-09-26 22:27:57 +0300

    Added my cv website to the project and more infor on footer

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
         <div class="wrapper">
-            <p class="text-center">Online Second Hand Books Purchase and Delivery System: Developed and Debugged by:<a href="https://jeconia-auma.github.io/web/cv/"> Okoth Jeconia Auma</a></p>
+            <p class="text-center">Online Second Hand Books Purchase and Delivery System: Developed and Debugged by:<a href="../developer_details/cv/index.html"> Okoth Jeconia Auma</a></p>
             <br>
             <h3>You can find me on social media platforms</h3>
             <a href="mailto: jeconiaauma@gmail.com" class="blocker"><i class="fa-solid fa-envelope"></i> Mail: jeconiaauma@gmail.com</a>
@@ -11,7 +11,7 @@
             <a href="https://www.linkedin.com/in/jeconia-auma" class="blocker"><i class="fa-brands fa-linkedin"></i> Jeconia Auma</a>
             <a href="https://github.com/jeconia-auma" class="blocker"><i class="fa-brands fa-github"></i> jeconia-auma</a>
             <br><br>
-            <a href="https://jeconia-auma.github.io/web/cv/"><i class="fa-solid fa-earth"></i> Click Here to view my portfolio Website</a>
+            <a href="../developer_details/cv/index.html"><i class="fa-solid fa-earth"></i> Click Here to view my portfolio Website</a>
         </div>
     </div>
 </body>
